36c7a9553c082a61ec83a7c335729351d7c53268
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2018-04-30 11:16:33 +0100

    Initialise helm

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 openssl aes-256-cbc -K $encrypted_058f1ad78f7f_key -iv $encrypted_058f1ad78f7f_iv -in deploy-key.rsa.enc -out deploy-key.rsa -d
 chmod 0400 deploy-key.rsa
 
+helm init --client-only
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
