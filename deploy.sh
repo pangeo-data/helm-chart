3af5f7fe15b22ebf27bbcb8a3171e04e7e4b0f1b
Guillaume Eynard-Bontemps <g.eynard.bontemps@gmail.com>
2019-03-05 13:10:46 +0100

    Switch to calver autopopulated with chartpress (#85)

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,10 @@ set -eu
 openssl aes-256-cbc -K $encrypted_058f1ad78f7f_key -iv $encrypted_058f1ad78f7f_iv -in deploy-key.rsa.enc -out deploy-key.rsa -d
 set -x
 chmod 0400 deploy-key.rsa
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 helm init --client-only
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 helm dependency update pangeo
 export GIT_SSH_COMMAND="ssh -i ${PWD}/deploy-key.rsa"
-chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --publish-chart --push
+chartpress --commit-range ${TRAVIS_COMMIT_RANGE} --publish-chart $@
 git diff
